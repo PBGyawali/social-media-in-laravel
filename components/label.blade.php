9332498fc951cc6b2277ebc33932250331ad360c
Prakhar Gyawali <38656630+PBGyawali@users.noreply.github.com>
2023-01-30 11:23:01 +0100

    Delete components directory

--- components/label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'text-center block font-medium text-md text-gray-700 mb-3']) }}>
-    {{ $value ?? $slot }}
-</label>
--- components/replies.blade.php
+++
@@ -1,6 +0,0 @@
-    <div class="commentbox reply clearfix"><!-- reply -->
-        <div id="profilepic_<?= $reply->id?>">
-            <img src="<?= $reply->profile_image ?>" alt="" height="40px" width="40px" class="profile_pic rounded-circle">
-        </div>
-        @include('reply_info')
-    </div>
